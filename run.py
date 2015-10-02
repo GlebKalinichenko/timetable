118483e6b6ab93c9619e30d10320b410ba8bb106
Yegorov <yegorov0725@yandex.ru>
2015-10-03 01:13:27 +0300

    heroku fix

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from timetable import app
 import os  
-port = int(os.environ.get('PORT', 33507)) 
-app.run(host='0.0.0.0', port=port) #debug=True
+#port = int(os.environ.get('PORT', 33507)) 
+app.run() #debug=True
