83170ad68fbe779267e8ca248a6abe7211cf4a5d
Yegorov <yegorov0725@yandex.ru>
2015-10-07 23:38:59 +0300

    Fix Server name variable

--- timetable/__init__.py
+++ timetable/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from timetable.api import api_part
+import os
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = 'localhost:5000'
+app.config['SERVER_NAME'] = os.environ["SERVER_NAME"] # 'localhost:5000'
 
 import timetable.controllers
 import timetable.api.controllers
