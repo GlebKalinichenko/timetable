337d0ae10cb0be56e13e70a5aac7f68cca3be09c
Yegorov <yegorov0725@yandex.ru>
2015-10-13 19:38:22 +0300

    Some change

--- timetable/api/controllers.py
+++ timetable/api/controllers.py
@@ -1,6 +1,6 @@
 from timetable.api import api_part
 import os
-from flask import Response
+from flask import Response, request
 from functools import wraps
 
 def returns_json(f):
@@ -18,8 +18,18 @@ def index():
 def index2():
     return Response('{"hello": "Hello World!"}', content_type='application/json; charset=utf-8')
 
-@api_part.route('/test')
+@api_part.route('/help')
 @returns_json
 def hello_world2():
     return "{\"key\": \"TimeTable " + os.environ["ENV_VAR"] + "\"}"
+
+#@api_part.route('/method/')    
+@api_part.route('/method/', methods=['GET', 'POST'], defaults={'api_method': "TEst"})
+@api_part.route('/method/<api_method>', methods=['GET', 'POST'])
+@returns_json
+def method(api_method=None):
+    if request.method == 'GET':
+        return "{\"key\": \"" + str(api_method) + "\"}"
+    else:
+        return "Post"
     
